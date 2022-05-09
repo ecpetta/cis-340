1da3902ab936a017db5b0426c6e133d0c62f44c9
ecpetta <69013599+ecpetta@users.noreply.github.com>
2022-05-09 03:30:32 +0000

    MyNinthProject
    changes to forecast and open_weather_map

--- App.js
+++ App.js
@@ -1,99 +1,99 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';// import
-import * as Sharing from 'expo-sharing';//import
 
-export default function App () {
+// This is a template project sent to students
+import React, { Component } from "react";
 
-  const [selectedImage, setSelectedImage] = React.useState(null)
+import { StyleSheet, Text, View, TextInput, Image} from "react-native";
 
+import Forecast from "./Forecast";
+import OpenWeatherMap from "./open_weather_map";
 
-  let openImagePickerAsync = async () => {
-    let permissionsResult = await ImagePicker.requestCameraPermissionsAsync();
-    
-    if (permissionsResult.granted===false){
-      alert("Permission is required!");
-      return;
-    }
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-    setSelectedImage({ localUri: pickerResult.uri })
+class WeatherForecast extends Component {
 
-  };
-
-  let openSharingDialogAsync =  async () => {
-    if (!(await Sharing.isAvailableAsync())){
-      alert('Sharing is not available on my phone');
-      return;
-    }
-    Sharing.shareAsync(selectedImage.localUri);
-  };
-
-  if (selectedImage !== null) {
-    return(
-      <View style={styles.container}>
-        <Image source={{ uri: selectedImage.localUri}} style={styles.selImage}/>
-
-        <TouchableOpacity onPress={openSharingDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}> Share my photo </Text>
-        </TouchableOpacity>
-      </View>
-    )
+  constructor (props){
+    super(props);
+    this.state = { zip: "", forecast:null};
   }
 
-return (
-    <View style={styles.container}>
-      <Image source={{uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/logo.png'}} style={styles.logo}/>
-        <Text style={styles.insts}>
-          Press the button below to select an image on your phoneeee!
-        </Text>
-      <TouchableOpacity style={styles.button} onPress={openImagePickerAsync} >
-          <Text style={styles.buttonText}>Pick an image</Text>
-      </TouchableOpacity>
+  _hndleTextChange = event => {
+    let zip = event.nativeEvent.text;
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      this.setState({ forecast: forecast})
 
-    </View>
-  );
+    });
+  };
+
+  render() {
+    let content= null;
+    if (this.state.forecast !== null){
+
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}/>
+      );
+    }
+  }
+  
+  
+  render() {
+    
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require("./assets/sky.jpg")}
+          resizeMode="cover"
+          style={styles.backdrop}
+        />
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current Weather For:
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing={this._hndleTextChange}
+                  underlineColorAndroid="transparent"
+                />
+              </View>
+            </View>
+
+            {content}
+          
+          </View>
+        
+      </View>
+    );
+  }
 }
 
+const baseFontSize = 16;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#FFFFE0"
+  container: { flex: 1, alignItems: "center", paddingTop: 30, backgroundColor: "#000000", },
+  backdrop: { flex: 1, flexDirection: "column" },
+  overlay: {
+    paddingTop: 5,
+     opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center"
   },
-
-  button: {
-    backgroundColor: "#778899",
-    padding: 20,
-    borderRadius: 5
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30
   },
-
-  insts: {
-    fontSize: 18,
-    color: "#87CEFA",
-    marginHorizontal: 15,
-    marginBottom: 10,
+  zipContainer: {
+    height: baseFontSize + 10,
+    borderBottomColor: "#FFFAF0",
+    borderBottomWidth: 3,
+    marginLeft: 18,
+   
   },
-
-  logo: {
-    wdith: 310,
-    height: 300,
-    marginBottom: 20
-  },
-
-  buttonText: {
-    fontSize: 20,
-    color: "#FFF"
-  },
-
-  selImage: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain'
-  }
-
+  zipCode: { flex: 1, width: 60, height: baseFontSize },
+  mainText: { fontSize: baseFontSize , color: "#FFFAF0" }
 });
+
+export default WeatherForecast;
